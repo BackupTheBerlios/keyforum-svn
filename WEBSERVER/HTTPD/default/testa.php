e756792fc9c670096a421ac6a7a39342ca5e06dc
ebutera <ebutera@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-11-15 20:42:27 +0000

    chiudi.php in testa.php

--- WEBSERVER/HTTPD/default/testa.php
+++ WEBSERVER/HTTPD/default/testa.php
@@ -129,7 +129,7 @@ function togglevis(id) {
   </div>
 </div>
 <div id="userlinks">
-  <p class="home"><b><a href="javascript:confirmThis('chiudi.pm')"><? echo $lang['userlink_close']; ?></a></b></p>
+  <p class="home"><b><a href="javascript:confirmThis('chiudi.php')"><? echo $lang['userlink_close']; ?></a></b></p>
   <p><b><a href="index.php"><? echo $lang['userlink_home']; ?></a></b> | <a href="gestip.php"><? echo $lang['userlink_ipman']; ?></a> | <a href="userlist.php?validati=1&nonvalidati=1"><? echo $lang['userlink_usrlst']; ?></a> | <a href="boardlist.pl"><? echo $lang['userlink_brdlst']; ?></a> | <? echo $lang['userlink_wlcm']; ?>
    <?php
       if ($sess_auth)
