6087d20e829a526b57fdef3b2dcbeebbe5285d48
mmorselli <mmorselli@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-11-22 21:45:35 +0000

    da lanciare dopo un SVN export per avere un keyforum funzionante

---
+++ post_svn_export.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+
+IF EXIST .svn\README.txt GOTO nosvn
+IF EXIST CORE\perl\bin\perl.exe GOTO nodupe
+
+ren COMMON\mysql\data\keyforum_base keyforum
+
+cd CORE\Perl
+ECHO Premi il pulsante EXTRACT quando ti viene richiesto...
+start /w perl.exe
+
+ECHO Terminato... ora esegui start.bat
+ECHO;
+pause
+
+goto fine
+
+
+:nosvn
+
+ECHO Non usare questo file direttamente nello snapshot SVN
+ECHO fai prima un Export (da tortoise: tasto destro TortoiseSVN / Export )
+ECHO in una nuova directory !
+ECHO;
+pause
+goto fine
+
+:nodupe
+ECHO Questo script e' gia' stato eseguito, fai eventualmente un nuovo
+ECHO Export in una nuova directory
+ECHO;
+pause
+goto fine
+
+
+:fine
