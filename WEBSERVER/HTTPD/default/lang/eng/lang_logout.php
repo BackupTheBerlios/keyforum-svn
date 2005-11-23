ed1f5e854e8deb18ad00e5622db0ccbffd8ce652
gulp <gulp@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-11-23 20:34:53 +0000

    fix errori di battitura

--- WEBSERVER/HTTPD/default/lang/eng/lang_logout.php
+++ WEBSERVER/HTTPD/default/lang/eng/lang_logout.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['logout_success']  = "You are logged off";
-$lang['logout_loginred']  = "Redirectiong to login page...";
+$lang['logout_loginred']  = "You are going to be redirected to the page you came from...";
 $lang['logout_notlogged']  = "You are not logged...";
 
 ?>
