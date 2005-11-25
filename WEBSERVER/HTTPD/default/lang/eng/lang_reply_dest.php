dee548aef76cbf81a8a9b6fffe47645d684f9417
gulp <gulp@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-11-25 22:52:41 +0000

    traduzione

--- WEBSERVER/HTTPD/default/lang/eng/lang_reply_dest.php
+++ WEBSERVER/HTTPD/default/lang/eng/lang_reply_dest.php
@@ -11,9 +11,9 @@ $lang['reply_wait']  = "Wait...";
 $lang['reply_thanks']  = "Thanks";
 $lang['reply_wait2']  = "Wait page loading...";
 $lang['reply_nowait']  = "(Or click here if you do not wish to wait)";
-$lang['reply_error']  = "Errore";
-$lang['reply_ok']  = "Risposta aggiunta correttamente";
-$lang['reply_error2']  = "Forum inesistente";
-$lang['reply_error3']  = "Errore sconosciuto: messaggio non accettato dal core";
-$lang['reply_nowait2']  = "Clicca qui per tornare al thread";
+$lang['reply_error']  = "Error";
+$lang['reply_ok']  = "Reply successfully inserted";
+$lang['reply_error2']  = "Board not found";
+$lang['reply_error3']  = "Unknown error: core cannot process message";
+$lang['reply_nowait2']  = "Click here to go back";
 ?>
