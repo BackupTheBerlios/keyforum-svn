68c57b5b702df8a2fea765e6e3885d2d3d5c2337
ebutera <ebutera@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-11-26 13:06:46 +0000

    conversione destnewmsg.pl

--- WEBSERVER/HTTPD/default/writenewmsg.php
+++ WEBSERVER/HTTPD/default/writenewmsg.php
@@ -31,7 +31,7 @@ $lang = $std->load_lang('lang_writenewmsg', $blanguage );
 
 <div id="FastReply" class='post2'>
 
-<form method="post" action="destnewmsg.pl">
+<form method="post" action="destnewmsg2.php">
 <input type=hidden name=sezid value="<?PHP print $_REQUEST["SEZID"] ?>">
 
   <div id="colo-sx" class='post2' align=center>
