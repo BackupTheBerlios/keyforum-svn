87f30989b21de8c1215f4d82c199112e0a3d4055
hierro <hierro@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-12-01 22:37:08 +0000

    LAYOUT SETUP : aggiornata la connessione al db usando i dati del file xml

--- WEBSERVER/HTTPD/setup/lib.php
+++ WEBSERVER/HTTPD/setup/lib.php
@@ -1,6 +1,23 @@
 <?php
 
-$server_sql = mysql_pconnect($_ENV[sql_host],$_ENV[sql_user],$_ENV[sql_passwd]);
+require_once "Config.php";
+$xmldata = new Config;
+
+$root =& $xmldata->parseConfig('http://'.$_SERVER['HTTP_HOST'].'/config/config.xml', 'XML');
+if (PEAR::isError($root)) {
+    die('Error reading XML config file: ' . $root->getMessage());
+}
+$settings = $root->toArray();
+// dati del db
+$_ENV['sql_host']=$settings['root']['conf']['DB']['host'];
+$_ENV['sql_user']=$settings['root']['conf']['DB']['dbuser'];
+$_ENV['sql_passwd']=$settings['root']['conf']['DB']['dbpassword'];
+$_ENV['sql_dbname']=$settings['root']['conf']['DB']['dbname'];
+$_ENV['sql_dbport']=$settings['root']['conf']['DB']['dbport'];
+if(!$_ENV['sql_dbport']){$_ENV['sql_dbport']="3306";}
+
+$server_sql = mysql_pconnect($_ENV['sql_host'].":".$_ENV['sql_dbport'],$_ENV['sql_user'],$_ENV['sql_passwd']);
+//$server_sql = mysql_pconnect($_ENV[sql_host],$_ENV[sql_user],$_ENV[sql_passwd]);
 if (!$server_sql) die ("Non riesco a connettermi al server MySQL");
 if ( !mysql_select_db($_ENV[sql_dbname]) ) die("Impossibile aprire il DataBase MySQL:".mysql_error().".</br>");
 
