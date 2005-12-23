9acc9c0025f42c6c2e103d950d5223ecdfd13df8
mmorselli <mmorselli@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-12-23 18:29:24 +0000

    ora anche le emoticon di base vengono lette dal db (usare nuove tabelle emoticons )

--- WEBSERVER/HTTPD/default/emoticons.php
+++ WEBSERVER/HTTPD/default/emoticons.php
@@ -1,38 +1,19 @@
-    <tr>
-     <td><a href="javascript:emot(':mellow:');"><img src="img/emoticons/mellow.gif" border="0" alt=":mellow:"></a></td>
-     <td><a href="javascript:emot(':huh:');"><img src="img/emoticons/huh.gif" border="0" alt=":huh:"></a></td>
-     <td><a href="javascript:emot('^_^');"><img src="img/emoticons/happy.gif" border="0" alt="^_^"></a></td>
-     <td><a href="javascript:emot(':o');"><img src="img/emoticons/ohmy.gif" border="0" alt=":o"></a></td>
-    </tr>
-    <tr>
-     <td><a href="javascript:emot(';)');"><img src="img/emoticons/wink.gif" border="0" alt=";)"></a></td>
-     <td><a href="javascript:emot(':P');"><img src="img/emoticons/tongue.gif" border="0" alt=":P"></a></td>
-     <td><a href="javascript:emot(':D');"><img src="img/emoticons/biggrin.gif" border="0" alt=":D"></a></td>
-     <td><a href="javascript:emot(':lol:');"><img src="img/emoticons/laugh.gif" border="0" alt=":lol:"></a></td>
-    </tr>
-    <tr>
-     <td><a href="javascript:emot('B)');"><img src="img/emoticons/cool.gif" border="0" alt="B)"></a></td>
-     <td><a href="javascript:emot(':rolleyes:');"><img src="img/emoticons/rolleyes.gif" border="0" alt=":rolleyes:"></a></td>
-     <td><a href="javascript:emot('-_-');"><img src="img/emoticons/sleep.gif" border="0" alt="-_-"></a></td>
-     <td><a href="javascript:emot('<_<');"><img src="img/emoticons/dry.gif" border="0" alt="<_<"></a></td>
-    </tr>
-    <tr>
-     <td><a href="javascript:emot(':)');"><img src="img/emoticons/smile.gif" border="0" alt=":)"></a></td>
-     <td><a href="javascript:emot(':wub:');"><img src="img/emoticons/wub.gif" border="0" alt=":wub:"></a></td>
-     <td><a href="javascript:emot(':angry:');"><img src="img/emoticons/mad.gif" border="0" alt=":angry:"></a></td>
-     <td><a href="javascript:emot(':(');"><img src="img/emoticons/sad.gif" border="0" alt=":("></a></td>
-    </tr>
-    <tr>
-     <td><a href="javascript:emot(':unsure:');"><img src="img/emoticons/unsure.gif" border="0" alt=":unsure:"></a></td>
-     <td><a href="javascript:emot(':wacko:');"><img src="img/emoticons/wacko.gif" border="0" alt=":wacko:"></a></td>
-     <td><a href="javascript:emot(':blink:');"><img src="img/emoticons/blink.gif" border="0" alt=":blink:"></a></td>
-     <td><a href="javascript:emot(':excl:');"><img src="img/emoticons/ph34r.gif" border="0" alt=":excl:"></a></td>
-    </tr>
-     <tr>
-         <td>&nbsp;</td>
-         <td>&nbsp;</td>
-         <td>&nbsp;</td>
-         <td>&nbsp;</td>
-        </tr>
-        <tr>
+<?
+ $SNAME=$_ENV['sesname'];
+  $query="SELECT id,typed,image,internal from {$SNAME}_emoticons WHERE enabled AND clickable";
+  $res=mysql_query($query) or die(mysql_error());
+  
+  $emocol=4; // numero colonne emoticon
+
+  $colcont=0;	
+   while ($row = mysql_fetch_assoc($res))
+     {
+      if (!$colcont) { echo "<tr>"; }
+      if ($row['internal']) {$emoadr="showemo.php?id={$row['id']}";} else {$emoadr="img/emoticons/{$row['image']}";}
+    echo "<td><img src='$emoadr' border='0' valign='absmiddle' style='cursor:hand;cursor:pointer;' onclick='javascript:emot(\"{$row['typed']}\")' alt='{$row['typed']}'></td>";
+    $colcont++;
+    if ($colcont == $emocol) {echo "</tr>";$colcont=0;}
+    }	
+    
+?>
     <tr><td colspan="4" align="center"><a href='javascript:' onclick='emopopup()'>Mostra tutte</a></td></tr>
