b6a3118682f76eabedae1aa270023a7dead49d33
gulp <gulp@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2005-12-28 13:51:53 +0000

    traduzioni

---
+++ WEBSERVER/HTTPD/default/lang/eng/lang_optionsuser.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['optusr_welcome']  = "Welcome to your control panel";
+$lang['optusr_msg']  = "update successful";
+$lang['optusr_errmsg']  = "Unknown error";
+$lang['optusr_intopt']  = "International option";
+$lang['optusr_lang']  = "Language";
+$lang['optusr_time']  = "Server time";
+$lang['optusr_bopt']  = "Board options";
+$lang['optusr_ppp']  = "Number of posts to show for each topic page";
+$lang['optusr_tpp']  = "Number of topics to show for each forum page";
+$lang['optusr_showsign']  = "Show client signature";
+$lang['optusr_update']  = "Update";
+
+?>
---
+++ WEBSERVER/HTTPD/default/lang/ita/lang_optionsuser.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['optusr_welcome']  = "Benvenuto nel Pannelo di Controllo";
+$lang['optusr_msg']  = "modifiche avvenute con successo";
+$lang['optusr_errmsg']  = "Errore imprevisto";
+$lang['optusr_intopt']  = "Impostazioni Internazionali";
+$lang['optusr_lang']  = "Lingua";
+$lang['optusr_time']  = "Ora del server";
+$lang['optusr_bopt']  = "Impostazioni Forum";
+$lang['optusr_ppp']  = "Numero di reply per pagina";
+$lang['optusr_tpp']  = "Numero di discussioni per pagina";
+$lang['optusr_showsign']  = "Mostra la firma del client";
+$lang['optusr_update']  = "Aggiorna";
+
+?>
