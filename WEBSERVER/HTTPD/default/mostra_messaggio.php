341ea514a1f2103580e8dbe6f4e302a7fac19ddc
dapuzz <dapuzz@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2006-01-05 23:29:56 +0000

    fix

--- WEBSERVER/HTTPD/default/mostra_messaggio.php
+++ WEBSERVER/HTTPD/default/mostra_messaggio.php
@@ -63,7 +63,7 @@ if(($userdata->LEVEL)  OR ($MSG->autore==$mio_nick))
   // visualizzo le firme ?
   if($userdata->HIDESIG) { $MSG->firma=""; } else { $MSG->firma = secure_v($MSG->firma);}
   // visualizzo lgli avatar
-  if($userdata->HIDEAVATAR { $MSG->avatar=""; }
+  if($userdata->HIDEAVATAR) { $MSG->avatar="" ;}
   
   echo<<<EOF
 <table width='100%' border='0' cellspacing='1' cellpadding='3'>
