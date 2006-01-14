54695d40950c8bb322d40da6269095c52e61d20a
mmorselli <mmorselli@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2006-01-14 21:10:45 +0000

    una index altrettanto grezza per setup

---
+++ WEBSERVER/HTTPD/setup/index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>KEYFORUM SETUP</title>
+</head>
+
+<body bgcolor="#FFFFFF">
+
+<p align="center"><b><font face="Verdana" size="6">KEYFORUM SETUP</font></b></p>
+<p align="center">&nbsp;</p>
+<p align="center"><b><a href="addboard.php">IMPORTA UNA BOARD</a></b></p>
+<p align="center"><b><a href="newboard.php">CREA UNA NUOVA BOARD</a></b></p>
+
+</body>
+
+</html>
