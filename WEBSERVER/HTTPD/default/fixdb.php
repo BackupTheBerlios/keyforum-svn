beb852f49a554534548c66e531acc5eb620e08f4
mmorselli <mmorselli@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2006-01-20 19:37:31 +0000

    fixdb ora fixa tutte le sessioni presenti

--- WEBSERVER/HTTPD/default/fixdb.php
+++ WEBSERVER/HTTPD/default/fixdb.php
@@ -1,6 +1,8 @@
 <?
 include ("testa.php"); # Librerie per creare la connessione MySQL
 
+$lastver="0.43";
+
 // nascondo gli errori mysql
 $db->hide_errors();
 
@@ -9,62 +11,67 @@ echo "<br><br><center><b>";
 // ******* V 310 **********
 $rev="310";
 echo "<font color=blue>AGGIORNAMENTO PER VERSIONE $rev</font><br>";
-
-$db->query("alter table intkf_localmember add column  `LEVEL` tinyint(2) unsigned NOT NULL default '0';");
-$db->query("alter table keyfo_localmember add column  `LEVEL` tinyint(2) unsigned NOT NULL default '0';");
-$db->query("alter table tstkf_localmember add column  `LEVEL` tinyint(2) unsigned NOT NULL default '0';");
-
-// test
-$EZSQL_ERROR="";
-$db->get_var("select LEVEL from keyfo_localmember");
-if (!$EZSQL_ERROR)
+foreach($config['WEBSERVER'] as $prefix)
 {
-echo "<br>DB Aggiornato alla versione 0.43 rev $rev<br><br><br>";
-} else {
-echo "<br><font color=red><b>errore aggiornando a 0.43 rev $rev</b></font><br>";
+$db->query("alter table {$prefix['SesName']}_localmember add column  `LEVEL` tinyint(2) unsigned NOT NULL default '0';");
+OutSess($prefix['SesName']);
 }
+echo "</font>";
+TestDB($lastver,$rev);
+
 
 
 // ******* V 375 **********
 $rev="375";
 echo "<font color=blue>AGGIORNAMENTO PER VERSIONE $rev</font><br>";
 
-$db->query("alter table intkf_localmember add column  `IS_AUTH` tinyint(1) unsigned NOT NULL default '0';");
-$db->query("alter table keyfo_localmember add column  `IS_AUTH` tinyint(1) unsigned NOT NULL default '0';");
-$db->query("alter table tstkf_localmember add column  `IS_AUTH` tinyint(1) unsigned NOT NULL default '0';");
-
-// test
-$EZSQL_ERROR="";
-$db->get_var("select IS_AUTH from keyfo_localmember");
-if (!$EZSQL_ERROR)
+foreach($config['WEBSERVER'] as $prefix)
 {
-echo "<br>DB Aggiornato alla versione 0.43 rev $rev<br><br><br>";
-} else {
-echo "<br><font color=red><b>errore aggiornando a 0.43 rev $rev</b></font><br>";
+$db->query("alter table {$prefix['SesName']}_localmember add column  `IS_AUTH` tinyint(1) unsigned NOT NULL default '0';");
+OutSess($prefix['SesName']);
 }
+TestDB($lastver,$rev);
+
 
 
 // ******* V 466 **********
 $rev="466";
 echo "<font color=blue>AGGIORNAMENTO PER VERSIONE $rev</font><br>";
 
-$db->query("alter table intkf_localmember add column  `HIDEAVATAR` tinyint(1) unsigned NOT NULL default '0';");
-$db->query("alter table keyfo_localmember add column  `HIDEAVATAR` tinyint(1) unsigned NOT NULL default '0';");
-$db->query("alter table tstkf_localmember add column  `HIDEAVATAR` tinyint(1) unsigned NOT NULL default '0';");
-echo "<br>DB Aggiornato alla versione 0.43 rev $rev<br><br><br>";
+foreach($config['WEBSERVER'] as $prefix)
+{
+$db->query("alter table {$prefix['SesName']}_localmember add column  `HIDEAVATAR` tinyint(1) unsigned NOT NULL default '0';");
+OutSess($prefix['SesName']);
+}
+TestDB($lastver,$rev);
+
+
+
 
 //*********V 470x colonne emoticons *******
 $rev="470";
 echo "<font color=blue>AGGIORNAMENTO PER VERSIONE $rev</font><br>";
 
-$db->query("alter table intkf_localmember add column  `EMOCOL` tinyint(1) unsigned NOT NULL default '4';");
-$db->query("alter table keyfo_localmember add column  `EMOCOL` tinyint(1) unsigned NOT NULL default '4';");
-$db->query("alter table tstkf_localmember add column  `EMOCOL` tinyint(1) unsigned NOT NULL default '4';");
-echo "<br>DB Aggiornato alla versione 0.43 rev $rev<br><br><br>";
-
+foreach($config['WEBSERVER'] as $prefix)
+{
+$db->query("alter table {$prefix['SesName']}_localmember add column  `EMOCOL` tinyint(1) unsigned NOT NULL default '4';");
+OutSess($prefix['SesName']);
+}
+TestDB($lastver,$rev);
 
 
 echo "</b></center>";
 
 include ("end.php");
+
+function TestDB($lastver,$rev)
+{
+echo "<br>DB Aggiornato alla versione $lastver rev $rev<br><br><br>";
+}
+
+function OutSess($sess)
+{
+echo "<font color=violet>$sess</font> ";
+}
+
 ?>
