bf34643a016875c333b458d167109a95641de4cb
gulp <gulp@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2006-01-23 16:02:24 +0000

    fix...  :D

--- WEBSERVER/HTTPD/default/lang/eng/lang_close.php
+++ WEBSERVER/HTTPD/default/lang/eng/lang_close.php
@@ -3,8 +3,8 @@
 $lang['close_info1'] = "Error with core communication!";
 $lang['close_info2'] = "You can now close KeyForum!";
 $lang['close_closew'] = "You can close this page.";
-$lang['close_disabled_title'] = "Unable to process you request";
-$lang['close_disabled_body'] = "Sorry but keyforum can't stop unless the multiuser feature is enabled - Help Gulp! :D";
+$lang['close_disabled_title'] = "Unable to process your request";
+$lang['close_disabled_body'] = "Your request to close Keyforum can't be fulfilled, others users on this machine may be using it.";
 $lang['restart'] = "Restarting KeyForum, wait some seconds before refresh the page...";
 
 
