d83eeb3ba9f05b1bf6e24f6ae71317638a6bb86f
mmorselli <mmorselli@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2006-01-26 09:12:58 +0000

    eliminato un PAUSE fastidioso in fase di riavvio

--- COMMON/script/chkdir.bat
+++ COMMON/script/chkdir.bat
@@ -1,6 +1,6 @@
 @echo off
 ECHO DIRECTORY CHECK
-IF EXIST "D:\bidone\bakaforum\newtest\KeyForum\WEBSERVER\Apache\conf\keyforum.conf" GOTO fine
+IF EXIST "E:\keyforum\newcore\KeyForum\WEBSERVER\Apache\conf\fakefile.null" GOTO fine
 ECHO KEYFORUM NEEDS CONFIGURATION
 ECHO;
 install_keyforum.bat
