8d08b84a8643ab12b5c4928012ad1c1527ad41e6
luftwaffer <luftwaffer@ef4d2a0b-8705-0410-a3a2-e0782237b35c>
2006-01-29 17:33:34 +0000

    Mi ero dimenticato questi file

--- WEBSERVER/HTTPD/setup/lang/eng/lang_index.php
+++ WEBSERVER/HTTPD/setup/lang/eng/lang_index.php
@@ -3,6 +3,7 @@
 $lang['index_title']  = "KEYFORUM SETUP";
 $lang['index_importbrd']  = "IMPORT A NEW BOARD";
 $lang['index_createnewbrd']  = "CREATE A NEW BOARD";
+$lang['index_managews']  = "MANAGE WEBSERVER";
 $lang['index_addon']  = "ADDON CONFIG";
 $lang['index_restart']  = "RESTART KEYFORUM";
 
